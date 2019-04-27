68ee1edaf9c542b57a59731f8ee6d287e3fc1366
petavy@gmx.net <petavy@gmx.net>
2019-04-27 21:19:20 +0200

    dicom indexing

--- models/bucket.go
+++ models/bucket.go
@@ -1,5 +1,11 @@
 package models
 
+import (
+	"context"
+	"github.com/go-pg/pg/orm"
+	"time"
+)
+
 //go:generate templater -sr Class=Bucket;class=document -i ../service/database/storm_class.go -o ../service/database/storm_bucket.go
 //go:generate templater -sr Class=Bucket;class=document -i ../service/database/badger_class.go -o ../service/database/badger_bucket.go
 //go:generate templater -sr Class=Bucket;class=document -i ../service/database/mongo_class.go -o ../service/database/mongo_bucket.go
@@ -7,10 +13,25 @@ package models
 
 type Bucket struct {
 	Base     `storm:"inline"`
-	Uid      string            `storm:"unique"`
+	Uid      string            `sql:",unique" storm:",unique"`
 	Prop     map[string]string `sql:",hstore"`
 	FileName []string          `sql:",array"`
 	FileType []string          `sql:",array"`
 	FileLen  []int64           `sql:",array"`
 	FileHash []string          `sql:",array"`
+}
+
+func NewBucket() Bucket {
+	b := Bucket{}
+	b.Prop = make(map[string]string)
+
+	return b
+}
+
+func (b *Bucket) BeforeInsert(c context.Context, db orm.DB) error {
+	if b.CreatedAt.IsZero() {
+		b.CreatedAt = time.Now()
+	}
+
+	return nil
 }
