4c7e05a31f64ac289a91a1633466a74e5e0731bb
mpetavy <mpetavy>
2022-09-15 12:55:54 +0200

    scan WordInfo added

--- utils/ocr.go
+++ utils/ocr.go
@@ -2,16 +2,15 @@ package utils
 
 import (
 	"bufio"
+	"bytes"
 	"flag"
 	"fmt"
+	"github.com/mpetavy/common"
 	"io"
 	"os"
 	"os/exec"
 	"strconv"
 	"strings"
-	"time"
-
-	"github.com/mpetavy/common"
 )
 
 var (
@@ -27,14 +26,14 @@ func init() {
 
 	tesseractPath = flag.String("tesseract.path", tpath, "Tesseract path")
 	tesseractLanguage = flag.String("tesseract.language", "deu", "Tesseract language")
-	ocrOrientationTimeout = flag.Int("ocr.orientation.timeout", 30000, "OCR orientation timeout")
-	ocrScanTimeout = flag.Int("ocr.scan.timeout", 50000, "OCR scan timeout")
+	ocrOrientationTimeout = flag.Int("ocr.orientation.timeout", 3000, "OCR orientation timeout")
+	ocrScanTimeout = flag.Int("ocr.scan.timeout", 5000, "OCR scan timeout")
 }
 
-func processText(imageFile string) (string, error) {
+func ScanText(imageFile string) (string, error) {
 	cmd := exec.Command(*tesseractPath, imageFile, "stdout", "-l", *tesseractLanguage)
 
-	ba, err := common.NewWatchdogCmd(cmd, time.Millisecond*time.Duration(*ocrScanTimeout))
+	ba, err := common.NewWatchdogCmd(cmd, common.MillisecondToDuration(*ocrScanTimeout))
 	if common.Error(err) {
 		return "", err
 	}
@@ -42,10 +41,82 @@ func processText(imageFile string) (string, error) {
 	return string(ba), nil
 }
 
-func processOrientation(imageFile string) (Orientation, error) {
+type WordInfo struct {
+	Level    int
+	PageNum  int
+	BlockNum int
+	ParNum   int
+	LineNum  int
+	WordNum  int
+	Left     int
+	Top      int
+	Width    int
+	Height   int
+	Conf     int
+	Text     string
+}
+
+func intValue(s string) int {
+	v, err := strconv.Atoi(s)
+	common.Panic(err)
+
+	return v
+}
+
+func ScanWordInfos(imageFile string) ([]WordInfo, error) {
+	cmd := exec.Command(*tesseractPath, imageFile, "-", "-l", *tesseractLanguage, "tsv")
+
+	ba, err := common.NewWatchdogCmd(cmd, common.MillisecondToDuration(*ocrScanTimeout))
+	if common.Error(err) {
+		return nil, err
+	}
+
+	l := []WordInfo{}
+	ok := false
+
+	scanner := bufio.NewScanner(bytes.NewReader(ba))
+	for scanner.Scan() {
+		line := scanner.Text()
+		cols := strings.Fields(line)
+
+		if len(cols) < 12 {
+			continue
+		}
+
+		if !ok {
+			_, err = strconv.Atoi(cols[0])
+			if err != nil {
+				continue
+			}
+
+			ok = true
+		}
+
+		wi := WordInfo{
+			Level:    intValue(cols[0]),
+			PageNum:  intValue(cols[1]),
+			BlockNum: intValue(cols[2]),
+			ParNum:   intValue(cols[3]),
+			LineNum:  intValue(cols[4]),
+			WordNum:  intValue(cols[5]),
+			Left:     intValue(cols[6]),
+			Top:      intValue(cols[7]),
+			Width:    intValue(cols[8]),
+			Height:   intValue(cols[9]),
+			Conf:     intValue(cols[10]),
+			Text:     cols[11],
+		}
+
+		l = append(l, wi)
+	}
+
+	return l, nil
+}
+
+func ScanOrientation(imageFile string) (Orientation, error) {
 	cmd := exec.Command(*tesseractPath, imageFile, "stdout", "--psm", "0")
 
-	ba, err := common.NewWatchdogCmd(cmd, time.Millisecond*time.Duration(*ocrOrientationTimeout))
+	ba, err := common.NewWatchdogCmd(cmd, common.MillisecondToDuration(*ocrOrientationTimeout))
 	if common.Error(err) {
 		return ORIENTATION_0, fmt.Errorf("cannot find orientation")
 	}
@@ -93,7 +164,7 @@ func processOrientation(imageFile string) (Orientation, error) {
 }
 
 func Ocr(imageFile string) (string, Orientation, error) {
-	orientation, err := processOrientation(imageFile)
+	orientation, err := ScanOrientation(imageFile)
 	if err != nil {
 		return "", -1, nil
 	}
@@ -135,10 +206,18 @@ func Ocr(imageFile string) (string, Orientation, error) {
 		imageFile = tmpFile.Name()
 	}
 
-	txt, err := processText(imageFile)
-
+	txt, err := ScanText(imageFile)
 	if common.Error(err) {
 		return "", -1, err
+	}
+
+	wis, err := ScanWordInfos(imageFile)
+	if common.Error(err) {
+		return "", -1, err
+	}
+
+	for _, wi := range wis {
+		fmt.Printf("%+v\n", wi)
 	}
 
 	return txt, orientation, nil
